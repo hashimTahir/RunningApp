8643f657a93641776309b189c7b35fa4de8d5a37
hashim <hashimtahir@live.com>
2020-08-24 23:15:53 +0500

    Queries and dao completed.
    Signed-off-by: hashim <hashimtahir@live.com>

---
+++ app/src/main/java/com/hashim/runningapp/repository/local/Queries.kt
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2020/  8/ 24.  Created by Hashim Tahir
+ */
+
+package com.hashim.runningapp.repository.local
+
+interface Queries {
+    companion object {
+        const val H_RUNS_ALL_SORTED_BY_DATE =
+            "SELECT * FROM RUNNINGTABLE ORDER BY hTimeInMills DESC"
+        const val H_RUNS_ALL_SORTED_BY_TIME_IN_MILLS =
+            "SELECT * FROM RUNNINGTABLE ORDER BY hTimeInMills DESC"
+        const val H_RUNS_ALL_SORTED_BY_CALORIES_BURNT =
+            "SELECT * FROM RUNNINGTABLE ORDER BY hCaloriesBurnt DESC"
+        const val H_RUNS_ALL_SORTED_BY_DISTANCE_IN_METERS =
+            "SELECT * FROM RUNNINGTABLE ORDER BY hDistanceInMeters DESC"
+        const val H_RUNS_ALL_SORTED_BY_AVERAGE_SPEED =
+            "SELECT * FROM RUNNINGTABLE ORDER BY hAverageSpeedInKms DESC"
+
+        const val H_GET_TOTAL_RUNNING_TIME =
+            "SELECT SUM(hTimeInMills) FROM RUNNINGTABLE"
+
+        const val H_GET_TOTAL_CALORIES_BURNT =
+            "SELECT SUM(hCaloriesBurnt) FROM RUNNINGTABLE"
+
+        const val H_GET_TOTAL_DISTANCE =
+            "SELECT SUM(hDistanceInMeters) FROM RUNNINGTABLE"
+
+        const val H_GET_AVERAGE_SPEED =
+            "SELECT AVG(hAverageSpeedInKms) FROM RUNNINGTABLE"
+
+
+
+    }
+}
---
+++ app/src/main/java/com/hashim/runningapp/repository/local/RunDao.kt
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2020/  8/ 24.  Created by Hashim Tahir
+ */
+
+package com.hashim.runningapp.repository.local
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+
+@Dao
+interface RunDao {
+    @Insert(
+        onConflict = OnConflictStrategy.REPLACE
+    )
+    suspend fun hInsertRunItem(run: Run)
+
+    @Delete
+    suspend fun hDeleteRunItem(run: Run)
+
+    /*Live data dosent work with coroutine*/
+    @Query(Queries.H_RUNS_ALL_SORTED_BY_DATE)
+    fun hGetAllRunsSortedByDate(): LiveData<List<Run>>
+
+    @Query(Queries.H_RUNS_ALL_SORTED_BY_TIME_IN_MILLS)
+    fun hGetAllRunsSortedByTimeInMills(): LiveData<List<Run>>
+
+    @Query(Queries.H_RUNS_ALL_SORTED_BY_CALORIES_BURNT)
+    fun hGetAllRunsSortedByCaloriesBurnt(): LiveData<List<Run>>
+
+    @Query(Queries.H_RUNS_ALL_SORTED_BY_DISTANCE_IN_METERS)
+    fun hGetAllRunsSortedByDistanceInMeters(): LiveData<List<Run>>
+
+    @Query(Queries.H_RUNS_ALL_SORTED_BY_AVERAGE_SPEED)
+    fun hGetAllRunsSortedByAverageSpeed(): LiveData<List<Run>>
+
+    @Query(Queries.H_GET_TOTAL_RUNNING_TIME)
+    fun hGetTotalRunningTimeInMillis(): LiveData<Long>
+
+    @Query(Queries.H_GET_TOTAL_CALORIES_BURNT)
+    fun hGetTotalCaloriesBurnt(): LiveData<Int>
+
+    @Query(Queries.H_GET_TOTAL_DISTANCE)
+    fun hGetTotalDistance(): LiveData<Int>
+
+    @Query(Queries.H_GET_AVERAGE_SPEED)
+    fun hGetAverage(): LiveData<Float>
+}
