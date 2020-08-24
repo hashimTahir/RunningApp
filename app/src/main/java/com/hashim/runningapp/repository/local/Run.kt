52c412c12699b7147a2579c3803de3dc426868b7
hashim <hashimtahir@live.com>
2020-08-24 22:32:33 +0500

    Run data entity class completed to store data in db.
    Signed-off-by: hashim <hashimtahir@live.com>

---
+++ app/src/main/java/com/hashim/runningapp/repository/local/Run.kt
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2020/  8/ 24.  Created by Hashim Tahir
+ */
+
+package com.hashim.runningapp.repository.local
+
+import android.graphics.Bitmap
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(
+    tableName = "RunningTable"
+)
+data class Run(
+    var hPreviewImage: Bitmap? = null,
+    var hTimStamp: Long = 0L,
+    var hAverageSpeedInKms: Float = 0F,
+    var hDistanceInMeters: Int = 0,
+    var hTimeInMills: Long = 0L,
+    var hCaloriesBurnt: Int = 0
+) {
+    @PrimaryKey(autoGenerate = true)
+    var hId: Int? = null
+}
