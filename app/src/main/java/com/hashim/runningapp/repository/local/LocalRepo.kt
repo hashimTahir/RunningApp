0d2df0efeaa01420222388ca9e4e9b8ba744576e
hashim <hashimtahir@live.com>
2020-08-25 11:45:11 +0500

    LocalRepo complete.
    Signed-off-by: hashim <hashimtahir@live.com>

--- app/src/main/java/com/hashim/runningapp/repository/local/LocalRepo.kt
+++ app/src/main/java/com/hashim/runningapp/repository/local/LocalRepo.kt
@@ -4,3 +4,58 @@
 
 package com.hashim.runningapp.repository.local
 
+import androidx.lifecycle.LiveData
+import com.hashim.runningapp.db.Run
+import com.hashim.runningapp.db.RunDao
+import javax.inject.Inject
+
+class LocalRepo @Inject constructor(
+    val hRunDao: RunDao
+) {
+    suspend fun hInsertRun(run: Run) {
+        hRunDao.hInsertRunItem(run)
+    }
+
+
+    suspend fun hDeleteRunItem(run: Run) {
+        hRunDao.hDeleteRunItem(run)
+    }
+
+    fun hGetAllRunsSortedByDate(): LiveData<List<Run>> {
+        return hRunDao.hGetAllRunsSortedByDate()
+    }
+
+    fun hGetAllRunsSortedByTimeInMills(): LiveData<List<Run>> {
+        return hRunDao.hGetAllRunsSortedByTimeInMills()
+    }
+
+    fun hGetAllRunsSortedByCaloriesBurnt(): LiveData<List<Run>> {
+        return hRunDao.hGetAllRunsSortedByCaloriesBurnt()
+
+    }
+
+    fun hGetAllRunsSortedByDistanceInMeters(): LiveData<List<Run>> {
+        return hRunDao.hGetAllRunsSortedByDistanceInMeters()
+    }
+
+    fun hGetAllRunsSortedByAverageSpeed(): LiveData<List<Run>> {
+        return hRunDao.hGetAllRunsSortedByAverageSpeed()
+    }
+
+    fun hGetTotalRunningTimeInMillis(): LiveData<Long> {
+        return hRunDao.hGetTotalRunningTimeInMillis()
+    }
+
+    fun hGetTotalCaloriesBurnt(): LiveData<Int> {
+        return hRunDao.hGetTotalCaloriesBurnt()
+    }
+
+    fun hGetTotalDistance(): LiveData<Int> {
+        return hRunDao.hGetTotalDistance()
+    }
+
+    fun hGetAverage(): LiveData<Float> {
+        return hRunDao.hGetAverage()
+    }
+
+}
