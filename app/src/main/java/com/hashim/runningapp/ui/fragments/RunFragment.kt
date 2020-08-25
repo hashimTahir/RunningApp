6e1442ea1f797b01b584ca740fe154dcc5f036ec
hashim <hashimtahir@live.com>
2020-08-25 12:03:32 +0500

    Fragments and layout files created.
    Signed-off-by: hashim <hashimtahir@live.com>

---
+++ app/src/main/java/com/hashim/runningapp/ui/fragments/RunFragment.kt
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2020/  8/ 25.  Created by Hashim Tahir
+ */
+
+package com.hashim.runningapp.ui.fragments
+
+import androidx.fragment.app.Fragment
+import com.hashim.runningapp.R
+
+
+class RunFragment : Fragment(R.layout.fragment_run) {
+
+
+}
---
+++ app/src/main/java/com/hashim/runningapp/ui/fragments/TrackingFragment.kt
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2020/  8/ 25.  Created by Hashim Tahir
+ */
+
+package com.hashim.runningapp.ui.fragments
+
+import androidx.fragment.app.Fragment
+import com.hashim.runningapp.R
+
+
+class TrackingFragment : Fragment(R.layout.fragment_tracking) {
+
+
+}
