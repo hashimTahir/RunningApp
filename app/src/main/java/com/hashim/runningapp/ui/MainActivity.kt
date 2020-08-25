f41ef635968dda529595d90e0f9cfad3fe86a201
hashim <hashimtahir@live.com>
2020-08-25 22:18:57 +0500

    Nav graph created and fragments added.
    Signed-off-by: hashim <hashimtahir@live.com>

--- app/src/main/java/com/hashim/runningapp/ui/MainActivity.kt
+++ app/src/main/java/com/hashim/runningapp/ui/MainActivity.kt
@@ -5,23 +5,48 @@
 package com.hashim.runningapp.ui
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.hashim.runningapp.R
 import com.hashim.runningapp.db.RunDao
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.activity_main.*
 import timber.log.Timber
 import javax.inject.Inject
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
 
     @Inject
     lateinit var hRunDao: RunDao
 
+    private lateinit var hNavHostFragment: NavHostFragment
+    private lateinit var hNavController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         Timber.d("Runn Dao ${hRunDao.hashCode()}")
+
+        hInitNavHostFragment()
+
+    }
+
+    private fun hInitNavHostFragment() {
+        hNavHostFragment = supportFragmentManager
+            .findFragmentById(R.id.hNavHostFragment) as NavHostFragment
+
+        hNavController = hNavHostFragment.navController
+        hNavController.setGraph(R.navigation.home_nav_graph)
+        NavigationUI.setupWithNavController(bottomNavigationView, hNavController)
+        bottomNavigationView.setOnNavigationItemSelectedListener(this)
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        TODO("Not yet implemented")
     }
 }
