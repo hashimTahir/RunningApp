b3187dc525c481a908d893b4fcae5f08b6714f15
hashim <hashimtahir@live.com>
2020-08-26 13:28:12 +0500

    Mapview integration completed.
    Signed-off-by: hashim <hashimtahir@live.com>

--- app/src/main/java/com/hashim/runningapp/ui/fragments/TrackingFragment.kt
+++ app/src/main/java/com/hashim/runningapp/ui/fragments/TrackingFragment.kt
@@ -4,11 +4,52 @@
 
 package com.hashim.runningapp.ui.fragments
 
-import androidx.fragment.app.Fragment
+import android.os.Bundle
+import android.view.View
+import com.google.android.gms.maps.GoogleMap
 import com.hashim.runningapp.R
+import kotlinx.android.synthetic.main.fragment_tracking.*
 
 
 class TrackingFragment : BaseFragment(R.layout.fragment_tracking) {
+    private var hGoogleMap: GoogleMap? = null
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        mapView?.onCreate(savedInstanceState)
+        mapView.getMapAsync {
+            hGoogleMap = it
+        }
+    }
 
+    override fun onStart() {
+        super.onStart()
+        mapView?.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView?.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView?.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapView?.onStop()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView?.onLowMemory()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        mapView?.onSaveInstanceState(outState)
+
+    }
 
 }
