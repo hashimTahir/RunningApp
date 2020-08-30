51a680deaedae73a2ba59323f082f5731357ebf7
hashim <hashimtahir@live.com>
2020-08-31 00:08:02 +0500

    Logic in view model added to sort the run. with mediatior live data.
    Signed-off-by: hashim <hashimtahir@live.com>

---
+++ app/src/main/java/com/hashim/runningapp/utils/SortType.kt
@@ -0,0 +1,9 @@
+/*
+ * Copyright (c) 2020/  8/ 30.  Created by Hashim Tahir
+ */
+
+package com.hashim.runningapp.utils
+
+enum class SortType {
+    H_DATE, H_RUNNING_TIME, H_AVG_SPEED, H_DISTANCE, H_CALORIES_BURNED
+}
--- app/src/main/java/com/hashim/runningapp/viewmodels/MainViewModel.kt
+++ app/src/main/java/com/hashim/runningapp/viewmodels/MainViewModel.kt
@@ -5,17 +5,70 @@
 package com.hashim.runningapp.viewmodels
 
 import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.hashim.runningapp.db.Run
 import com.hashim.runningapp.repository.local.LocalRepo
+import com.hashim.runningapp.utils.SortType.*
 import kotlinx.coroutines.launch
 
 class MainViewModel @ViewModelInject constructor(
     private val hLocalRepo: LocalRepo
 ) : ViewModel() {
 
-    val hRunsSortedByDate = hLocalRepo.hGetAllRunsSortedByDate()
+    private val hRunsSortedByDate = hLocalRepo.hGetAllRunsSortedByDate()
+    private val hRunsSortedByDistance = hLocalRepo.hGetAllRunsSortedByDistanceInMeters()
+    private val hRunsSortedByCaloriesBurnt = hLocalRepo.hGetAllRunsSortedByCaloriesBurnt()
+    private val hRunsSortedByTimeinMillis = hLocalRepo.hGetAllRunsSortedByTimeInMills()
+    private val hRunsSortedByAverageSpeed = hLocalRepo.hGetAllRunsSortedByAverageSpeed()
+
+    private val hSortType = H_DATE
+
+    /*Mediator live data allows to merge several live datas togather
+    * also allows to write custom logic to emit what data is to be emitted.*/
+    val hMediatorLiveData = MediatorLiveData<List<Run>>()
+
+    init {
+        /*For any change in the source change lambda is called*/
+        hMediatorLiveData.addSource(hRunsSortedByDate) { result ->
+            if (hSortType.equals(H_DATE)) {
+                result?.let {
+                    hMediatorLiveData.value = result
+                }
+            }
+        }
+
+        hMediatorLiveData.addSource(hRunsSortedByDistance) { result ->
+            if (hSortType.equals(H_DISTANCE)) {
+                result?.let {
+                    hMediatorLiveData.value = result
+                }
+            }
+        }
+        hMediatorLiveData.addSource(hRunsSortedByCaloriesBurnt) { result ->
+            if (hSortType.equals(H_CALORIES_BURNED)) {
+                result?.let {
+                    hMediatorLiveData.value = result
+                }
+            }
+        }
+        hMediatorLiveData.addSource(hRunsSortedByTimeinMillis) { result ->
+            if (hSortType.equals(H_RUNNING_TIME)) {
+                result?.let {
+                    hMediatorLiveData.value = result
+                }
+            }
+        }
+        hMediatorLiveData.addSource(hRunsSortedByAverageSpeed) { result ->
+            if (hSortType.equals(H_AVG_SPEED)) {
+                result?.let {
+                    hMediatorLiveData.value = result
+                }
+            }
+
+        }
+    }
 
     fun hInsertRun(run: Run) {
         viewModelScope.launch {
