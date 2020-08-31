defcd9573b84bf4379843af120b6652434f1eaa5
hashim <hashimtahir@live.com>
2020-08-31 06:11:45 +0500

    Setting up bar chart in StatsFragment completed.
    Signed-off-by: hashim <hashimtahir@live.com>

--- app/src/main/java/com/hashim/runningapp/ui/fragments/StatsFragment.kt
+++ app/src/main/java/com/hashim/runningapp/ui/fragments/StatsFragment.kt
@@ -4,10 +4,16 @@
 
 package com.hashim.runningapp.ui.fragments
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.View
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import com.github.mikephil.charting.components.XAxis
+import com.github.mikephil.charting.data.BarData
+import com.github.mikephil.charting.data.BarDataSet
+import com.github.mikephil.charting.data.BarEntry
 import com.hashim.runningapp.R
 import com.hashim.runningapp.utils.TrackingUtils
 import com.hashim.runningapp.viewmodels.StatsViewModel
@@ -24,6 +30,31 @@ class StatsFragment : Fragment(R.layout.fragment_stats) {
         super.onViewCreated(view, savedInstanceState)
         hSubscribeObservers()
 
+    }
+
+
+    private fun hSetupBarChart() {
+        barChart.xAxis.apply {
+            position = XAxis.XAxisPosition.BOTTOM
+            setDrawLabels(false)
+            axisLineColor = Color.WHITE
+            textColor = Color.WHITE
+            setDrawGridLines(false)
+        }
+        barChart.axisLeft.apply {
+            axisLineColor = Color.WHITE
+            textColor = Color.WHITE
+            setDrawGridLines(false)
+        }
+        barChart.axisRight.apply {
+            axisLineColor = Color.WHITE
+            textColor = Color.WHITE
+            setDrawGridLines(false)
+        }
+        barChart.apply {
+            description.text = "Avg Speed Over Time"
+            legend.isEnabled = false
+        }
     }
 
     private fun hSubscribeObservers() {
@@ -58,6 +89,22 @@ class StatsFragment : Fragment(R.layout.fragment_stats) {
                 it?.let {
                     val hTotalCaloreies = "$it Calories"
                     tvTotalCalories.text = hTotalCaloreies
+                }
+            }
+
+        hStatsViewModel.hRunsSortedByDate
+            .observe(viewLifecycleOwner) {
+                it?.let {
+                    val hAllAvgSpeed = it.indices.map { i ->
+                        BarEntry(i.toFloat(), it.get(i).hAverageSpeedInKms)
+                    }
+                    val hBarDataSet = BarDataSet(hAllAvgSpeed, "Avg Speed over Time")
+                        .apply {
+                            valueTextColor = Color.WHITE
+                            color = ContextCompat.getColor(requireContext(), R.color.colorAccent)
+                        }
+                    barChart.data = BarData(hBarDataSet)
+                    barChart.invalidate()
                 }
             }
     }
